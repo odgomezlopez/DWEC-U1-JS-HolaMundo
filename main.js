8a20c33fd8b55f6cdfc36f11de43862b274e488c
odgomezlopez <odgomezlopez@gmail.com>
2022-09-19 21:56:34 +0200

    Commit inicial

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict"
+
+//Se indica que cuando se cargue la pagina se lance la funcion init()
+window.addEventListener('load', init);
+
+//Funcion de inicializacion
+function init() {
+  console.log('Hola mundo');
+
+
+  //Ejemplo de error
+  const name='error';
+  console.log('Hola ' + nombre);
+}
+
+/*function checkEsLintCompability(){
+  // Instantiate PaymentRequest
+  const req = new PaymentRequest();
+
+  // Instantiate IntersectionObserver
+  const observer = new IntersectionObserver(() => {});
+
+  const foo = Object.values({});
+}*/
